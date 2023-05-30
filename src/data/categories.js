27ef6458d708ed83522291948c2b3474a13840ae
Anthony Allois <anthonyallois@gmail.com>
2023-05-30 17:23:04 -0300

    feat: Adding dummy data

---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+    name: 'Euro Games',
+    description: 'This is category 1',
+  },
+  {
+    id: 2,
+    name: 'War Games',
+    description: 'This is category 2',
+  },
+  {
+    id: 3,
+    name: 'Estrategia ',
+    description: 'This is category 3',
+  },
+  {
+    id: 4,
+    name: 'Aventura',
+    description: 'This is category 4',
+  },
+];
---
+++ src/data/products.js
@@ -0,0 +1,42 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+    description: 'This is product 1',
+    imgUrl: 'https://picsum.photos/200/300',
+    categoryId: 1,
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200,
+    description: 'This is product 2',
+    imgUrl: 'https://picsum.photos/200/300',
+    categoryId: 2,
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300,
+    description: 'This is product 3',
+    imgUrl: 'https://picsum.photos/200/300',
+    categoryId: 1,
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    price: 400,
+    description: 'This is product 4',
+    imgUrl: 'https://picsum.photos/200/300',
+    categoryId: 3,
+  },
+  {
+    id: 5,
+    name: 'Product 5',
+    price: 500,
+    description: 'This is product 5',
+    imgUrl: 'https://picsum.photos/200/300',
+    categoryId: 4,
+  },
+];
