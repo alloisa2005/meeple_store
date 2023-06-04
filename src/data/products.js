5f3d965b08aa279ef9f5af0781330159bd4dc9e1
Anthony Allois <anthonyallois@gmail.com>
2023-06-04 19:59:36 -0300

    feat: Creacion de categoryItem component

--- src/data/products.js
+++ src/data/products.js
@@ -1,42 +1,56 @@
 export const PRODUCTS = [
   {
     id: 1,
-    name: 'Product 1',
-    price: 100,
+    name: 'Viticulture',
+    price: 3200,
     description: 'This is product 1',
-    imgUrl: 'https://picsum.photos/200/300',
+    imgUrl:
+      'https://res.cloudinary.com/dayyupv1u/image/upload/v1685919224/games/viticulture_bg_ylzqbo.png',
     categoryId: 1,
   },
   {
     id: 2,
-    name: 'Product 2',
+    name: 'Oh My Goods!',
     price: 200,
     description: 'This is product 2',
-    imgUrl: 'https://picsum.photos/200/300',
-    categoryId: 2,
-  },
-  {
-    id: 3,
-    name: 'Product 3',
-    price: 300,
-    description: 'This is product 3',
-    imgUrl: 'https://picsum.photos/200/300',
-    categoryId: 1,
-  },
-  {
-    id: 4,
-    name: 'Product 4',
-    price: 400,
-    description: 'This is product 4',
-    imgUrl: 'https://picsum.photos/200/300',
+    imgUrl:
+      'https://res.cloudinary.com/dayyupv1u/image/upload/v1685919170/games/ohMyGoods_bg_mxuouf.png',
     categoryId: 3,
   },
   {
+    id: 3,
+    name: 'Horrified',
+    price: 300,
+    description: 'This is product 3',
+    imgUrl:
+      'https://res.cloudinary.com/dayyupv1u/image/upload/v1685919062/games/horrified_bg_skspnz.png',
+    categoryId: 5,
+  },
+  {
+    id: 4,
+    name: 'Stone Age',
+    price: 400,
+    description: 'This is product 4',
+    imgUrl:
+      'https://res.cloudinary.com/dayyupv1u/image/upload/v1685919000/games/stoneAge_bg_zpbztw.png',
+    categoryId: 1,
+  },
+  {
     id: 5,
-    name: 'Product 5',
+    name: 'Catan',
     price: 500,
     description: 'This is product 5',
-    imgUrl: 'https://picsum.photos/200/300',
+    imgUrl:
+      'https://res.cloudinary.com/dayyupv1u/image/upload/v1685918908/games/catan_bg_llsnu3.png',
     categoryId: 4,
+  },
+  {
+    id: 6,
+    name: 'Dice Tower',
+    price: 100,
+    description: 'This is product 5',
+    imgUrl:
+      'https://res.cloudinary.com/dayyupv1u/image/upload/v1685919553/games/diceTower_p4d59f.png',
+    categoryId: 6,
   },
 ];
